fdb14ddbdaf721c8da17a2b3f8f277f548a197f4
Haisen Zhao <haisenzhao@gmail.com>
2023-07-25 19:13:55 +0800

    update

--- post/main.cpp
+++ post/main.cpp
@@ -5,137 +5,6 @@
 using namespace std;
 using namespace PGL;
 
-void Check_Not_Included(std::string root_path)
-{
-	auto Get_CGAL_Functions = [](const std::string& path, 
-		VectorStr1& funct_titles, VectorStr1& funct_lines)
-	{
-		std::ifstream file(path);
-		for (std::string line; std::getline(file, line); )
-		{
-			if (Functs::StringContain(line, "CGAL"))
-			{
-				auto title = line.substr(line.find("CGAL"), line.find("(") - line.find("CGAL"));
-				funct_titles.push_back(title);
-				funct_lines.push_back(line);
-				//std::cerr << title << std::endl;
-			}
-		}
-		file.close();
-
-	};
-
-	//parse geom.h
-	VectorStr1 funct_titles;
-	VectorStr1 funct_lines;
-
-	Get_CGAL_Functions(root_path+"ppgl\\geom.h", funct_titles, funct_lines);
-
-	VectorStr1 sfc_titles;
-	VectorStr1 sfc_lines;
-	Get_CGAL_Functions("E:\\Task2\\SmartCFS\\cgal\\cgalpackage.h",sfc_titles, sfc_lines);
-
-	int debug_int = 0;
-
-	ofstream file("E:\\out.txt");
-	int i = 0;
-	for (auto& sfc_title : sfc_titles)
-	{
-		bool b = true;
-		for (auto& title : funct_titles)
-		{
-			if (Functs::StringContain(title, sfc_title))
-			{
-				b = false;
-				break;
-			}
-		}
-
-		std::cerr << i << std::endl;
-
-		if (b)
-		{
-			//void CGAL_2D_Convex_Hulls(Vector2d1 & vec, Vector2d1 & hull_points);
-			auto line = sfc_lines[i];
-
-			if (!Functs::StringContain(line, "(")|| !Functs::StringContain(line, ")"))
-			{
-				std::cerr << line << std::endl;
-				Functs::MAssert(line);
-			}
-
-			if (Functs::SplitStr(line, "(").size() != 2 || Functs::SplitStr(line, ")").size() != 2 || Functs::StringContain(line, "const"))
-			{
-				file << "extern \"C\" PPGL_EXPORT " << line << std::endl;
-			}
-			else
-			{
-				auto str_0 = Functs::SplitStr(line, "(")[0];
-				auto str_1 = Functs::SplitStr(line, "(")[1];
-				auto str_2 = Functs::SplitStr(str_1, ")")[0];
-				auto str_3 = Functs::SplitStr(str_1, ")")[1];
-
-				//std::string path, Vector3d1 &ves, Vector3d1 &ners
-
-				str_2 = Functs::StringReplace(str_2, "&", "");
-				str_2 = Functs::StringReplace(str_2, ",   ", ",");
-				str_2 = Functs::StringReplace(str_2, ",  ", ",");
-				str_2 = Functs::StringReplace(str_2, ", ", ",");
-				
-				auto paras = Functs::SplitStr(str_2,",");
-				str_2 = "";
-				int ii = 0;
-				for (auto para : paras)
-				{
-					auto defs=Functs::SplitStr(para, " ");
-
-					if (defs.size() == 2)
-					{
-						if (ii == paras.size() - 1)
-							str_2 += "const " + defs[0] + "& " + defs[1];
-						else
-							str_2 += "const " + defs[0] + "& " + defs[1] + ", ";
-					}
-					else
-					{
-						if (defs.size() == 1)
-						{
-							if (ii == paras.size() - 1)
-								str_2 += "const " + defs[0] + "& ";
-							else
-								str_2 += "const " + defs[0] + "& " + ", ";
-						}
-						else
-						{
-							if (ii == paras.size() - 1)
-								str_2 += "const " + para;
-							else
-								str_2 += "const " + para + ", ";
-						}
-					}
-
-					ii++;
-				}
-
-				//str_2 = Functs::StringReplace(str_2, ",", ", const");
-
-				auto str = str_0 + "(" + str_2 + ")" + str_3;
-				//const
-				//&
-				//line = Functs::StringReplace(line, "(", "(const ");
-				//line = Functs::StringReplace(line, ",", ", const");
-				file << "extern \"C\" PPGL_EXPORT " << str << std::endl;
-			}
-		}
-		i++;
-	}
-	
-	//Functs::CerrLine();
-	file.close();
-	system("pause");
-
-};
-
 void Generate_CGAL_H(std::string input_path, std::string output_path)
 {
 	//parse geom.h
@@ -250,7 +119,6 @@ void Generate_CGAL_H(std::string input_path, std::string output_path)
 	cgal_file.close();
 };
 
-
 int main(int argc, char* argv[])
 {
 	//get root path
@@ -275,10 +143,9 @@ int main(int argc, char* argv[])
 			if (Functs::DetectExisting(root_path + "port\\" + pd + "\\ppgl.exp"))
 				Functs::WinDel(root_path + "port\\" + pd + "\\ppgl.exp");
 		}
-
 	}
 
-	Functs::MAssert("Successfully finishing the post processing...",1.5);
+	Functs::MAssert("Successfully finishing the post processing...", 1.5);
 	return 0;
 }
 
